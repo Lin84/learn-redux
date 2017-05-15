c78ecd1d03b6625775c3cc823ce0d2b530c72b87
Linh Le Tuan <linh.tuan@actum.cz>
2017-05-15 15:06:16 +0200

    fix typo

--- client/store.js
+++ client/store.js
@@ -1,11 +1,11 @@
 import { createStore, compse } from 'redux';
-import { synHistoryWithStore } from 'react-router-redux';
+import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
 // import the root reducer
 import rootReducer from './reducers/index';
 
-import components from './data/comments';
+import comments from './data/comments';
 import posts from './data/posts';
 
 // create an object for the default data
@@ -16,6 +16,6 @@ const defaultState = {
 
 const store = createStore(rootReducer, defaultState);
 
-export const history = synHistoryWithStore(browserHistory, store);
+export const history = syncHistoryWithStore(browserHistory, store);
 
 export default store;
