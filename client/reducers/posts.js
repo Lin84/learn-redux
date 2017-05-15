3952d6d9158d70cd1080d6eba0f08f120aeedbbe
Linh Le Tuan <linh.tuan@actum.cz>
2017-05-15 14:54:35 +0200

    07 - All About Redux Reducers

---
+++ client/reducers/posts.js
@@ -0,0 +1,11 @@
+// a reducer takes in two things:
+// 1. the ation (info about what happened)
+// 2. copy of current state
+
+function posts(state = [], action) {
+    console.log(state, action);
+    return state;
+
+}
+
+export default posts;
