99366f0ecc98e9e9177318e56e46f43aad158aa2
Linh Le Tuan <linh.tuan@actum.cz>
2017-06-11 21:29:06 +0200

    14 - Displaying and Adding Comments

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 
-class Comment extends Component {
+class Comments extends Component {
+    static renderComment(comment, i) {
+        return (
+            <div className='comment' key={i}>
+                <p>
+                    <strong>{comment.user}</strong>
+                    {comment.text}
+                    <button className='remove-comment'>&times;</button>
+                </p>
+            </div>
+        )
+    }
+
     render() {
         return (
-            <div className='Comment'>
-                I am comment
+            <div className='comments'>
+                { this.props.postComments.map(Comments.renderComment)}
+                <form ref="commentForm" className="comment-form">
+                    <input type='text' ref="author" placeholder="author"/>
+                    <input type='text' ref="comment" placeholder="comment"/>
+                    <input type='submit' hidden/>
+                </form>
             </div>
         );
     }
 
 }
 
-export default Comment;
+export default Comments;
--- client/components/Single.js
+++ client/components/Single.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import Photo from './Photo';
-import Comment from './Comments';
+import Comments from './Comments';
 
 class Single extends Component {
     render() {
-        // index of the post
+        const { postId } = this.props.params;
+
+        // index of the post:
         const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
-        // get us the post
+        // get us the post:
         const post = this.props.posts[i];
+
+        const postComments = this.props.comments[postId] || [];
 
         return (
             <div className="single-photo">
                 <Photo i={i} post={post} {...this.props}></Photo>
-                <Comment />
+                <Comments postComments={postComments} />
             </div>
         );
     }
